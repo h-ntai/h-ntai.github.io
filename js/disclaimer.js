355eab973d584ba9dab4f43740f167637366391a
Türkischer Sniper <96152215+turkish-sniper@users.noreply.github.com>
2022-01-17 20:39:24 +0100

    Update disclaimer.js

--- js/disclaimer.js
+++ js/disclaimer.js
@@ -1,5 +1,5 @@
 const disclaimer = {
-    "text": "Einige Aussagen des Bots könnten eventuell beleidigend, homophob oder rassistisch wirken.\nKeine dieser Aussagen ist Ernst gemeint sondern fällt in die Kategorie Sarkasmus bzw. Schwarzer Humor.\nDurch das Hinzufügen des Bots zu einem Server und/oder auch durch der Nutzung des Bots,\nwilligt man automatisch ein,\ndass man jede Aussage mit Humor nehmen wird.\nFalls es einem trotzdem aus diversen Gründen nicht passen sollte,\nkann er gerne den Bot vom Server entfernen oder (falls ihm die Rechte dazu fehlen) den Server selber verlassen.\nVielen Dank für euer Verständnis."
+    "text": "Einige Aussagen des Bots könnten eventuell beleidigend, homophob oder rassistisch wirken.\nKeine dieser Aussagen ist Ernst gemeint sondern fällt in die Kategorie Sarkasmus bzw. Schwarzer Humor.\nDurch das Hinzufügen des Bots auf einen Server und/oder auch durch die Nutzung des Bots\nwilligt man automatisch ein,\ndass man jede Aussage mit Humor nehmen wird.\nFalls es einem trotzdem aus diversen Gründen nicht passen sollte,\nkann er gerne den Bot vom Server entfernen oder (falls ihm die Rechte dazu fehlen) den Server selber verlassen.\nVielen Dank für euer Verständnis."
 }
 
 document.getElementById("warn").innerText += disclaimer.text;
